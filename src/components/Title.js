a095d94b06f58af08963865279f38c996212e669
Arnau Silvestre <arnau.silvestredulcet@gartner.com>
2022-09-21 11:32:33 +0200

    Removed maxWidth

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import { capterraBlueColor, orangeColor } from "../Theme";
 
 export const Title = () => (
     <Box sx={{ gridColumn: '2/3', gridRow: '1/2' }}>
-        <h1 style={{ color: capterraBlueColor, margin: '0 0 32px', fontSize: '28px', lineHeight: '33.41px', maxWidth: '512px' }}>
+        <h1 style={{ color: capterraBlueColor, margin: '0 0 32px', fontSize: '28px', lineHeight: '33.41px' }}>
             <span style={{ color: orangeColor }}>/&nbsp;</span>
             Capterra helps you find the right software for your business
         </h1>
