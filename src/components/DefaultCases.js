a990cc61146d54979ed9847f01bc93631ff6765a
Ivan Ruiz <ivan.ruiz.delatorre@gmail.com>
2022-09-21 15:18:47 +0200

    Popular questions

--- src/components/DefaultCases.js
+++ src/components/DefaultCases.js
@@ -4,21 +4,46 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
-
+import SearchIcon from './SearchIcon'
 
 const someTexts = [
-    'I run a church and I need to hire a priest',
-    'I have an international team and we are looking to work closer'
+    'I need to monitor a project remotely',
+    'I need to create a school portal administration for students',
+    'I need a data compliant HR system to store the employees information',
+    'I want a free solution for my SMB to support my customer service team',
 ]
 
 export default function DefaultCases({onClickAtCase}) {
     return ( 
-        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <Box sx={{ width: '800px'}}>
+            <div
+                style={{
+                    color: "#93B4D8",
+                    fontFamily: 'Arial',
+                    fontStyle: 'normal',
+                    fontWeight: 400,
+                    fontSize: '32px',
+                    marginBottom: 12,
+                }}      
+            >Popular questions</div>
             <List>
                 {someTexts.map(text => (
                     <ListItem onClick={() => onClickAtCase(text)} disablePadding>
-                        <ListItemButton>
-                        <ListItemText primary={text} />
+                        <ListItemButton
+                            style={{
+                                borderBottom: '1px solid #607480',
+                                paddingBottom: '16px',
+                            }}
+                        >
+                            <div
+                                 style={{
+                                    color: "#F2F8FB",
+                                    fontSize: '22px',
+                                    display: 'flex',
+                                    flexDirection: 'row'
+                                }}
+                            >
+                                <div style={{ marginRight: 12}}><SearchIcon /></div> <div style={{ paddingTop: '5px'}}>{`"${text}"`}</div></div>
                         </ListItemButton>
                     </ListItem> 
                 ))}
